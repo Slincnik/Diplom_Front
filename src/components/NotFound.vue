6de30a40888a6d5f01a2dbb2bbf4986e305de1de
Slincnik <pattyshow21@mail.ru>
2023-06-08 19:29:32 +0300

    Editing not found page

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <h1>Not found</h1>
-  </div>
+  <v-card variant="outlined" elevation="3" class="pa-6 mx-auto" width="350" min-width="300" rounded>
+    <div class="mb-6 text-center text-h5">Страница не найдена.</div>
+    <div class="text-center">
+      Страница, на которую вы пытаетесь попасть, никогда не существовала в этой реальности или перекочевала в
+      параллельную вселенную.
+    </div>
+  </v-card>
 </template>
