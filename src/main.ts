2cd8b98adf99d49f7ffc73cb18be53d5af49aaa7
Slincnik <pattyshow21@mail.ru>
2023-06-08 12:31:27 +0300

    Added some new packages

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,50 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import Toast from "vue-toastification";
+import type { VueQueryPluginOptions } from "@tanstack/vue-query";
+import "vue-toastification/dist/index.css";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark",
+  },
+});
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+const vueQuerySettings: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  },
+};
+
+const toastOpt = {
+  timeout: 1500,
+  pauseOnFocusLoss: false,
+  pauseOnHover: false,
+  maxToasts: 3,
+};
+
 app.use(createPinia());
 app.use(router);
+app.use(VueQueryPlugin, vueQuerySettings);
+app.use(vuetify);
+app.use(Toast, toastOpt);
 
 app.mount("#app");
