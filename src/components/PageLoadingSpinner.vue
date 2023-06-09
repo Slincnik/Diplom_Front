151c5a6a4f5251b66f419c03f5c27150a8efcc5f
Slincnik <pattyshow21@mail.ru>
2023-06-09 13:29:00 +0300

    Added loading spinner when page is loading

---
+++ src/components/PageLoadingSpinner.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container class="fill-height h-screen" fluid justify="center" align="center">
+    <v-progress-circular size="large" class="mx-auto" indeterminate />
+  </v-container>
+</template>
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 const route = useRoute();
+import LoadingSpinner from "@/components/PageLoadingSpinner.vue";
 </script>
 
 <template>
-  <component :is="route.meta.layoutComponent">
-    <slot />
-  </component>
+  <template v-if="route.meta.layoutComponent">
+    <Transition name="fade" mode="out-in">
+      <component :is="route.meta.layoutComponent">
+        <slot />
+      </component>
+    </Transition>
+  </template>
+  <template v-else>
+    <LoadingSpinner />
+  </template>
 </template>
+
+<style>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease-out;
+}
+</style>
