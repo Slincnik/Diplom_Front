28d58cd0b5c119c1de73a176e01c88ec23ec8edd
Slincnik <pattyshow21@mail.ru>
2023-06-12 02:30:41 +0300

    remove lodash

--- src/modules/home/utils/orderConversationsOrGroups.ts
+++ src/modules/home/utils/orderConversationsOrGroups.ts
@@ -1,12 +1,19 @@
-import { orderBy } from "lodash";
 import type { Conversation, Group } from "../types/index.types";
 
-export const orderConversationsOrGroups = (dialog: Conversation[] | Group[]) => {
-  return orderBy(
-    dialog,
-    (item: Conversation) => {
-      return item.lastMessage ? item.lastMessage.created_at : item.created_at;
-    },
-    "desc",
-  ) as Conversation[] | Group[];
+export const orderConversationsOrGroups = (array: Conversation[] | Group[]) => {
+  return array
+    .map(item => item)
+    .sort((valueA, valueB) => {
+      if (valueA.lastMessage && valueB.lastMessage) {
+        const date1 = new Date(valueA.lastMessage.created_at).getTime();
+        const date2 = new Date(valueB.lastMessage.created_at).getTime();
+        return date2 - date1;
+      } else if (valueA.lastMessage) {
+        return -1;
+      } else if (valueB.lastMessage) {
+        return 1;
+      } else {
+        return new Date(valueB.created_at).getTime() - new Date(valueA.created_at).getTime();
+      }
+    });
 };
