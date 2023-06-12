189b9ddf66c82c1ab667b9ce69241aaf4f283e79
Slincnik <pattyshow21@mail.ru>
2023-06-13 02:32:33 +0300

    unnecessary main has been removed

--- src/layouts/AppLayoutDefault.vue
+++ src/layouts/AppLayoutDefault.vue
@@ -1,43 +1,41 @@
 <template>
-  <main>
-    <v-app>
-      <v-app-bar :elevation="2" rounded>
-        <v-app-bar-title> Messenger </v-app-bar-title>
-        <template v-if="user" v-slot:append>
-          <v-btn prepend-icon="mdi-view-dashboard" class="mr-2" :to="{ name: PAGES.DASHBOARD }"> Dashboard </v-btn>
-          <v-menu rounded min-width="200px">
-            <template v-slot:activator="{ props }">
-              <v-btn icon v-bind="props">
-                <v-avatar color="brown" size="large">
+  <v-app>
+    <v-app-bar :elevation="2" rounded>
+      <v-app-bar-title> Messenger </v-app-bar-title>
+      <template v-if="user" v-slot:append>
+        <v-btn prepend-icon="mdi-view-dashboard" class="mr-2" :to="{ name: PAGES.DASHBOARD }"> Dashboard </v-btn>
+        <v-menu rounded min-width="200px">
+          <template v-slot:activator="{ props }">
+            <v-btn icon v-bind="props">
+              <v-avatar color="brown" size="large">
+                <span class="text-h5">{{ getInitials(user.fullname) }}</span>
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-text>
+              <div class="mx-auto text-center">
+                <v-avatar size="large" color="brown">
                   <span class="text-h5">{{ getInitials(user.fullname) }}</span>
                 </v-avatar>
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-text>
-                <div class="mx-auto text-center">
-                  <v-avatar size="large" color="brown">
-                    <span class="text-h5">{{ getInitials(user.fullname) }}</span>
-                  </v-avatar>
-                  <h4 class="mt-2">{{ user.fullname }}</h4>
-                  <p class="text-caption mt-2" style="cursor: pointer" @click.prevent="copyLogin(user.login)">
-                    {{ user.login }}
-                  </p>
-                  <v-divider class="my-3"></v-divider>
-                  <v-btn rounded variant="text" to="/logout"> Выйти </v-btn>
-                </div>
-              </v-card-text>
-            </v-card>
-          </v-menu>
-        </template>
-      </v-app-bar>
-      <v-main>
-        <v-container class="fill-height" fluid justify="center" align="center">
-          <slot />
-        </v-container>
-      </v-main>
-    </v-app>
-  </main>
+                <h4 class="mt-2">{{ user.fullname }}</h4>
+                <p class="text-caption mt-2" style="cursor: pointer" @click.prevent="copyLogin(user.login)">
+                  {{ user.login }}
+                </p>
+                <v-divider class="my-3"></v-divider>
+                <v-btn rounded variant="text" to="/logout"> Выйти </v-btn>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-menu>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <v-container class="fill-height" fluid justify="center" align="center">
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
