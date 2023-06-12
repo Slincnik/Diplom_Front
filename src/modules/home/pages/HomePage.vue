1af707684cd8e3fe845893cc95fcad16e3dc6f7b
Slincnik <pattyshow21@mail.ru>
2023-06-12 21:43:01 +0300

    Added dialogId to query

--- src/modules/home/pages/HomePage.vue
+++ src/modules/home/pages/HomePage.vue
@@ -11,30 +11,44 @@
     </v-col>
     <v-divider vertical />
     <v-col sm="10">
-      <HistoryComponent v-if="currentDialogId" />
+      <HistoryComponent v-if="currentDialogId" :key="currentDialogId" />
       <div v-else class="fill-height d-flex flex-column justify-center align-center">Выберите чат</div>
     </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted } from "vue";
+import { onMounted, onUnmounted, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import { useQuery } from "@tanstack/vue-query";
 import { storeToRefs } from "pinia";
 import useDialogsStore from "@/stores/dialogs";
+
 import centra from "@/plugins/centrifuge";
+
 import type { MessagesFromCentrifugo } from "../types/index.types";
 // Components
 import Sidebar from "../components/Dialogs/SidebarComponent.vue";
 import HistoryComponent from "../components/Dialogs/History/HistoryComponent.vue";
 
 const dialogsStore = useDialogsStore();
+const router = useRouter();
+const route = useRoute();
 
 const { currentDialogId } = storeToRefs(dialogsStore);
 
 const { isLoading } = useQuery({
   queryKey: ["dialogs"],
   queryFn: dialogsStore.fetchDialogs,
+});
+
+watch(currentDialogId, newId => {
+  router.push({
+    query: {
+      ...route.query,
+      id: newId,
+    },
+  });
 });
 
 const listener = (event: KeyboardEvent) => {
