dbe410199be6ad9043d6651684d7c2cb279871a1
Slincnik <pattyshow21@mail.ru>
2023-06-12 21:39:58 +0300

    Messages component update

--- src/modules/home/components/Dialogs/History/MessagesComponent.vue
+++ src/modules/home/components/Dialogs/History/MessagesComponent.vue
@@ -1,33 +1,90 @@
 <template>
-  <div class="d-flex flex-column overflow-y-auto w-50" ref="messagesRef">
-    <MessageComponent v-for="item in messages" :key="item.id" :item="item" :userId="user!.id" />
+  <div class="message-container d-flex flex-column justify-end w-50" ref="messagesRef">
+    <v-fade-transition group>
+      <v-list-item
+        @click.prevent="consoled"
+        class="mb-4 rounded-lg"
+        v-for="item in dialog.messages"
+        :key="item.id"
+        :class="[
+          {
+            ['align-self-end align-end rounded-be-0 bg-blue']: item.sender.id === user?.id,
+            ['align-self-start align-start rounded-bs-0 bg-grey-darken-4']: item.sender.id !== user?.id,
+          },
+        ]"
+        :color="item.sender.id === user?.id ? 'blue' : 'gray'"
+        max-width="300px"
+      >
+        <span class="text-h7 py-3 text-white">
+          {{ item.body }}
+        </span>
+      </v-list-item>
+    </v-fade-transition>
   </div>
 </template>
 
 <script setup lang="ts">
-import { nextTick, onMounted, ref } from "vue";
+import { onUpdated, ref, onMounted, nextTick } from "vue";
 import useUserStore from "@/stores/user";
-
-import MessageComponent from "./Message/MessageComponent.vue";
-
-import type { Message, MessageGroup } from "@/modules/home/types/index.types";
+import type { Conversation, Group } from "@/modules/home/types/index.types";
 
 const userStore = useUserStore();
 
+const messagesRef = ref<HTMLElement | null>(null);
+const isFirstLoadingMessages = ref(true);
+
 const user = userStore.getUser;
 
-const messagesRef = ref<HTMLElement | null>(null);
-
-defineProps<{
-  messages: Message[] | MessageGroup[];
+const props = defineProps<{
+  scrollRef: HTMLElement | null;
+  dialog: Conversation | Group;
 }>();
 
-onMounted(async () => {
+const formatDate = (value: string) => {
+  return Intl.DateTimeFormat(navigator.language, {
+    day: "numeric",
+    year: "numeric",
+    month: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  }).format(new Date(value));
+};
+
+const scrollToTop = (behavior: "auto" | "smooth" = "auto") => {
   if (messagesRef.value) {
     const messages = messagesRef.value;
     nextTick(() => {
-      messages.scrollIntoView({ block: "end", behavior: "auto" });
+      messages.scrollIntoView({
+        block: "end",
+        behavior,
+      });
     });
   }
+};
+
+const consoled = () => {
+  const messages = props.scrollRef;
+  messages?.scrollTo({
+    top: 156,
+    behavior: "smooth",
+  });
+};
+
+onMounted(scrollToTop);
+
+onUpdated(() => {
+  nextTick(() => {
+    if (isFirstLoadingMessages.value) {
+      scrollToTop();
+      isFirstLoadingMessages.value = false;
+    }
+  });
 });
 </script>
+
+<style scoped>
+.message-container {
+  min-height: 100%;
+}
+</style>
