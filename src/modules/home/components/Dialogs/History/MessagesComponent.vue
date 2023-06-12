5a11a9e98b2d991da73b9e13c88b6b514150c70c
Slincnik <pattyshow21@mail.ru>
2023-06-12 21:40:43 +0300

    Added time to message

--- src/modules/home/components/Dialogs/History/MessagesComponent.vue
+++ src/modules/home/components/Dialogs/History/MessagesComponent.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="message-container d-flex flex-column justify-end w-50" ref="messagesRef">
+  <div class="message-container d-flex flex-column justify-end w-50">
     <v-fade-transition group>
       <v-list-item
-        @click.prevent="consoled"
         class="mb-4 rounded-lg"
         v-for="item in dialog.messages"
         :key="item.id"
@@ -18,20 +17,31 @@
         <span class="text-h7 py-3 text-white">
           {{ item.body }}
         </span>
+        <v-list-item-subtitle>
+          <p
+            class="text-white"
+            :class="{
+              'text-right': item.sender.id === user?.id,
+              'text-left': item.sender.id !== user?.id,
+            }"
+          >
+            {{ formatDate(item.created_at) }}
+          </p>
+        </v-list-item-subtitle>
       </v-list-item>
     </v-fade-transition>
+    <div ref="endMessagesRef" id="#scrollHere" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { onUpdated, ref, onMounted, nextTick } from "vue";
+import { ref, onMounted, nextTick, watch } from "vue";
 import useUserStore from "@/stores/user";
 import type { Conversation, Group } from "@/modules/home/types/index.types";
 
 const userStore = useUserStore();
 
-const messagesRef = ref<HTMLElement | null>(null);
-const isFirstLoadingMessages = ref(true);
+const endMessagesRef = ref<HTMLElement | null>(null);
 
 const user = userStore.getUser;
 
@@ -42,9 +52,6 @@ const props = defineProps<{
 
 const formatDate = (value: string) => {
   return Intl.DateTimeFormat(navigator.language, {
-    day: "numeric",
-    year: "numeric",
-    month: "numeric",
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
@@ -52,8 +59,8 @@ const formatDate = (value: string) => {
 };
 
 const scrollToTop = (behavior: "auto" | "smooth" = "auto") => {
-  if (messagesRef.value) {
-    const messages = messagesRef.value;
+  if (endMessagesRef.value) {
+    const messages = endMessagesRef.value;
     nextTick(() => {
       messages.scrollIntoView({
         block: "end",
@@ -63,24 +70,22 @@ const scrollToTop = (behavior: "auto" | "smooth" = "auto") => {
   }
 };
 
-const consoled = () => {
-  const messages = props.scrollRef;
-  messages?.scrollTo({
-    top: 156,
-    behavior: "smooth",
-  });
-};
-
 onMounted(scrollToTop);
 
-onUpdated(() => {
-  nextTick(() => {
-    if (isFirstLoadingMessages.value) {
-      scrollToTop();
-      isFirstLoadingMessages.value = false;
-    }
-  });
-});
+watch(
+  () => props.dialog.isLoaded,
+  isLoaded => {
+    if (!isLoaded) return;
+    scrollToTop();
+  },
+);
+
+// onUpdated(() => {
+//   if (isFirstLoadingMessages.value) {
+//     scrollToTop();
+//     isFirstLoadingMessages.value = false;
+//   }
+// });
 </script>
 
 <style scoped>
