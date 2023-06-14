13f282031a78dc4e09df48bab4480e60c1b9cb92
Slincnik <pattyshow21@mail.ru>
2023-06-14 04:45:57 +0300

    change to lodash orderBy

--- src/modules/messanger/utils/orderConversationsOrGroups.ts
+++ src/modules/messanger/utils/orderConversationsOrGroups.ts
@@ -1,18 +1,12 @@
+import { orderBy } from "lodash";
 import type { Conversation, Group } from "../types/index.types";
 
 export const orderConversationsOrGroups = (array: Conversation[] | Group[]) => {
-  return array.sort((valueA, valueB) => {
-    if (valueA.lastMessage && valueB.lastMessage) {
-      return (
-        new Date(valueB.lastMessage.created_at).getTimezoneOffset() -
-        new Date(valueA.lastMessage.created_at).getTimezoneOffset()
-      );
-    } else if (valueA.lastMessage) {
-      return -1; // Если только у a есть lastMessage (а у b нет), возвращается -1, указывая, что a должно стоять перед b в отсортированном массиве.
-    } else if (valueB.lastMessage) {
-      return 1; // Если только b имеет lastMessage (а a его нет), он возвращает 1, указывая, что b должен стоять перед a в отсортированном массиве.
-    } else {
-      return new Date(valueB.created_at).getTimezoneOffset() - new Date(valueA.created_at).getTimezoneOffset();
-    }
-  });
+  return orderBy(
+    array,
+    (item: Conversation | Group) => {
+      return item.lastMessage ? item.lastMessage.created_at : item.created_at;
+    },
+    "desc",
+  );
 };
