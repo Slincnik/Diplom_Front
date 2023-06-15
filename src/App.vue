fec0b820901b26a54760c9814487f4fa43495409
Slincnik <pattyshow21@mail.ru>
2023-06-16 00:14:00 +0300

    Remove error captured

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import { onErrorCaptured } from "vue";
-import { ref } from "vue";
 import { RouterView } from "vue-router";
 
 import AppLayout from "./layouts/AppLayout.vue";
-
-const isError = ref(false);
-
-onErrorCaptured((error, _, info) => {
-  if (info === "component event handler") return;
-  isError.value = true;
-});
 </script>
 
 <template>
-  <template v-if="isError">
-    <h2>Случилась ошибка, перезагрузите страницу</h2>
-  </template>
-  <AppLayout v-else>
+  <AppLayout>
     <RouterView />
   </AppLayout>
 </template>
