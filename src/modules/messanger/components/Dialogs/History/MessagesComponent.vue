15c041c03f1bb82ac3703074c12a783fdc0a5c50
Slincnik <pattyshow21@mail.ru>
2023-06-17 20:19:53 +0300

    Add user name, when is group and sender not u

--- src/modules/messanger/components/Dialogs/History/MessagesComponent.vue
+++ src/modules/messanger/components/Dialogs/History/MessagesComponent.vue
@@ -15,6 +15,9 @@
         max-width="300px"
         @contextmenu.prevent="(event: MouseEvent) => RMBClick(event, item)"
       >
+        <div v-if="dialog.type === 'group' && item.sender.id !== user?.id" class="text-wrap text-left text-white">
+          {{ item.sender.fullname }}
+        </div>
         <v-list-item v-if="item.response" class="pl-2 pr-0 itemReply">
           <v-list-item-title class="text-white text-wrap">
             {{ item.response.sender.fullname }}
