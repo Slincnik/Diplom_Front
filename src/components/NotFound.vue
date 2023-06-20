9fb8abe293797559ae4818adcf434ec9a7b65fed
Slincnik <pattyshow21@mail.ru>
2023-06-20 19:42:31 +0300

    Fix style 'Not Found' page

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-card variant="outlined" elevation="3" class="pa-6 mx-auto" width="350" min-width="300" rounded>
-    <div class="mb-6 text-center text-h5">Страница не найдена.</div>
-    <div class="text-center">
-      Страница, на которую вы пытаетесь попасть, никогда не существовала в этой реальности или перекочевала в
-      параллельную вселенную.
-    </div>
-  </v-card>
+  <div class="fill-height d-flex align-center">
+    <v-card variant="outlined" elevation="3" class="pa-6 mx-auto" width="350" min-width="300" rounded>
+      <div class="mb-6 text-center text-h5">Страница не найдена.</div>
+      <div class="text-center">
+        Страница, на которую вы пытаетесь попасть, никогда не существовала в этой реальности или перекочевала в
+        параллельную вселенную.
+      </div>
+    </v-card>
+  </div>
 </template>
